671401ec450bb8004062745c949cce50abb738d2
niemtec <niemtec@users.noreply.github.com>
2021-03-20 17:26:23 +0000

    Updated currency and added shopping cart logic

--- lib/product_row_item.dart
+++ lib/product_row_item.dart
@@ -50,7 +50,7 @@ class ProductRowItem extends StatelessWidget {
                   ),
                   const Padding(padding: EdgeInsets.only(top: 8)),
                   Text(
-                    '\$${product.price}',
+                    '\£${product.price}',
                     style: Styles.productRowItemPrice,
                   )
                 ],
--- lib/shopping_cart_tab.dart
+++ lib/shopping_cart_tab.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
-import 'styles.dart';
+import 'package:provider/provider.dart';
+
 import 'model/app_state_model.dart';
+import 'model/product.dart';
+import 'styles.dart';
 
 const double _kDateTimePickerHeight = 216;
 
@@ -19,6 +21,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
   String? location;
   String? pin;
   DateTime dateTime = DateTime.now();
+  final _currencyFormat = NumberFormat.currency(symbol: '\£');
 
   Widget _buildNameField() {
     return CupertinoTextField(
@@ -140,6 +143,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
       AppStateModel model) {
     return SliverChildBuilderDelegate(
       (context, index) {
+        final productIndex = index - 4;
         switch (index) {
           case 0:
             return Padding(
@@ -156,13 +160,52 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: _buildLocationField(),
             );
-          case 3: // ADD FROM HERE
+          case 3:
             return Padding(
               padding: const EdgeInsets.fromLTRB(16, 8, 16, 24),
               child: _buildDateAndTimePicker(context),
             );
           default:
-          // Do nothing. For now.
+            if (model.productsInCart.length > productIndex) {
+              return ShoppingCartItem(
+                index: index,
+                product: model.getProductById(
+                    model.productsInCart.keys.toList()[productIndex]),
+                quantity: model.productsInCart.values.toList()[productIndex],
+                lastItem: productIndex == model.productsInCart.length - 1,
+                formatter: _currencyFormat,
+              );
+            } else if (model.productsInCart.keys.length == productIndex &&
+                model.productsInCart.isNotEmpty) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          'Shipping '
+                          '${_currencyFormat.format(model.shippingCost)}',
+                          style: Styles.productRowItemPrice,
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Tax ${_currencyFormat.format(model.tax)}',
+                          style: Styles.productRowItemPrice,
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Total  ${_currencyFormat.format(model.totalCost)}',
+                          style: Styles.productRowTotal,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }
         }
         return null;
       },
@@ -189,5 +232,85 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
         );
       },
     );
+  }
+}
+
+class ShoppingCartItem extends StatelessWidget {
+  const ShoppingCartItem({
+    required this.index,
+    required this.product,
+    required this.lastItem,
+    required this.quantity,
+    required this.formatter,
+  });
+
+  final Product product;
+  final int index;
+  final bool lastItem;
+  final int quantity;
+  final NumberFormat formatter;
+
+  @override
+  Widget build(BuildContext context) {
+    final row = SafeArea(
+      top: false,
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          top: 8,
+          bottom: 8,
+          right: 8,
+        ),
+        child: Row(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.cover,
+                width: 40,
+                height: 40,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          product.name,
+                          style: Styles.productRowItemName,
+                        ),
+                        Text(
+                          '${formatter.format(quantity * product.price)}',
+                          style: Styles.productRowItemName,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      '${quantity > 1 ? '$quantity x ' : ''}'
+                      '${formatter.format(product.price)}',
+                      style: Styles.productRowItemPrice,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return row;
   }
 }
